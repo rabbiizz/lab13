9c13d95c255230872a3853dbdb464c1356ead47f
rabbiizz <Supakarn_k@cmu.ac.th>
2024-01-02 16:25:31 +0700

    lab13_1

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -17,3 +17,42 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+void stat(const double A[],int N,double B[]){
+    double sum1 = 0;
+    double sum2 = 0;
+    double sum3 = 0;
+    double sum4 = 1;
+    double sum5 = 0;
+    double max = A[0];
+    double min = A[0];
+    for(int i = 0; i < N; i++){
+        sum1 += A[i];
+    }
+    B[0] = sum1/N;
+    for(int i = 0; i < N; i++){
+        sum2 += pow(A[i],2);
+    }
+    for(int i = 0; i < N; i++){
+        sum3 += pow(B[0],2);
+    }
+    B[1] = sqrt((sum2-sum3)/N);
+    for(int i = 0; i < N; i++){
+        sum4 *= A[i];
+    }
+    B[2] = pow(sum4,1.0/N);
+    for(int i = 0; i < N; i++){
+        sum5 += 1.0/A[i];
+    }
+    B[3] = N/sum5;
+    for(int i = 0; i < N; i++){
+        if(A[i] > max){
+            max = A[i];
+        }
+        if(A[i] < min){
+            min = A[i];
+        }
+    }
+    B[4] = max;
+    B[5] = min;
+}
